d45383e76db9a56278a2c5045ce2d8e4ce686c21
TIMMS Daniel P <t016546e@student.staffs.ac.uk>
2018-01-30 12:30:50 +0000

    added animation for main menu

--- Assets/EscMenu.cs
+++ Assets/EscMenu.cs
@@ -2,16 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EscMenu : MonoBehaviour {
+public class EscMenu : MonoBehaviour 
+{
 
 	public GameObject pauseMenu;
+	public Animator animR; 
+	public Animator animS; 
+	public Animator animL; 
+	public Animator animO; 
+	public Animator animQ; 
 
-	void Update () {
+	void Awake()
+	{
+		animR = GetComponent<Animator> ();
+		animS = GetComponent<Animator> ();
+		animL = GetComponent<Animator> ();
+		animO = GetComponent<Animator> ();
+		animQ = GetComponent<Animator> ();
+	}
+
+	void Update () 
+	{
 
 		if(Input.GetKeyDown(KeyCode.Escape))
 		{
 			if (Time.timeScale == 1)
 			{
+				animR.SetTrigger ("Normal");	
+				animS.SetTrigger ("Normal");
+				animL.SetTrigger ("Normal");
+				animO.SetTrigger ("Normal");
+				animQ.SetTrigger ("Normal");
 				Time.timeScale = 0;
 				pauseMenu.SetActive (true);
 			}
