1f44f5a39db1af5be72a90be0f997dc4921f6df3
TIMMS Daniel P <t016546e@student.staffs.ac.uk>
2018-02-13 11:32:09 +0000

    added menu functions

--- Assets/EscMenu.cs
+++ Assets/EscMenu.cs
@@ -11,6 +11,8 @@ public class EscMenu : MonoBehaviour
 	public Animator animL; 
 	public Animator animO; 
 	public Animator animQ; 
+	bool isPaused;
+
 
 	void Awake()
 	{
@@ -19,29 +21,63 @@ public class EscMenu : MonoBehaviour
 		animL = GetComponent<Animator> ();
 		animO = GetComponent<Animator> ();
 		animQ = GetComponent<Animator> ();
+		isPaused = false;
 	}
 
 	void Update () 
 	{
 
-		if(Input.GetKeyDown(KeyCode.Escape))
+		if(Input.GetKeyUp(KeyCode.Escape))
 		{
-			if (Time.timeScale == 1)
+			if (!isPaused)
 			{
-				animR.SetTrigger ("Normal");	
-				animS.SetTrigger ("Normal");
-				animL.SetTrigger ("Normal");
-				animO.SetTrigger ("Normal");
-				animQ.SetTrigger ("Normal");
-				Time.timeScale = 0;
-				pauseMenu.SetActive (true);
+				StartCoroutine (waitAndSetScale());
+				//isPaused = true;
 			}
 			else
 			{
-				Time.timeScale = 1;
-				pauseMenu.SetActive (false);
+				StartCoroutine (resumeGameScale());
+				//isPaused = false;
 			}
 		}
+
 	}
+
+	IEnumerator waitAndSetScale() 
+	{
+		pauseMenu.SetActive (true);
+		animR.SetBool ("Normal", true);	
+		animS.SetBool("Normal", true);
+		animL.SetBool ("Normal", true);
+		animO.SetBool ("Normal", true);
+		animQ.SetBool ("Normal", true);
+
+		while(animQ.GetComponent<Animator> ().isInitialized)
+			yield return new WaitForSeconds(1);
+		
+		pauseMenu.SetActive (true);
+		//Time.timeScale = 0;
+	}
+		 
+	IEnumerator resumeGameScale()
+	{
+		Time.timeScale = 1;
+		Debug.Log (animR.GetInteger("speed"));
+
+		animR.SetBool ("Normal", false);
+		animS.SetBool ("Normal 0", true);
+		animL.SetBool ("Normal 0", true);
+		animO.SetBool ("Normal 0", true);
+		animQ.SetBool ("Normal 0", true);
+
+		while(!animQ.GetComponent<Animator> ().isInitialized)
+			yield return new WaitForSeconds(1);
+		
+		pauseMenu.SetActive (false);
+
+	}
+//
+//
+
 }
 
