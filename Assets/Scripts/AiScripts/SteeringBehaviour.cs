248366a9a392b1299e0a52a18709a2538a25cbbc
JONES Ashley M <j024030e@student.staffs.ac.uk>
2018-02-20 14:56:23 +0000

    began to re add some ai code to the new 3d version

---
+++ Assets/Scripts/AiScripts/SteeringBehaviour.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SteeringBehaviours
+{
+    public static class Steering
+    {
+        public enum behaviour
+        {
+            SEEK,
+            FLEE,
+            ARRIVE,
+            AVOID,
+            PURSUIT,
+            EVADE
+        };
+
+        // Binary steering
+        public static Vector3 Seek(Vector3 target, Vector3 me, float speed)
+        {
+            return (target - me).normalized * speed;
+        }
+
+
+        public static Vector3 Flee(Vector3 target, Vector3 me, float speed)
+        {
+            return (me - target).normalized * speed;
+        }
+
+        // Throttled steering
+        public static Vector3 Arrive(Vector3 target, Vector3 me, float speed, float sensitivity)
+        {
+            Vector3 diff = target - me;
+            float diffMag = diff.magnitude;
+
+            if (diffMag < sensitivity)
+            {
+                Vector3 returnVec;
+
+                returnVec = diff.normalized * (speed * (diffMag / sensitivity));
+
+                return returnVec;
+            }
+            else
+            {
+                return Seek(target, me, speed);
+            }
+        }
+
+        public static Vector3 Avoid(Vector3 target, Vector3 me, float speed, float sensitivity)
+        {
+            Vector3 diff = me - target;
+            float diffMag = diff.magnitude;
+
+            if (diffMag > sensitivity)
+            {
+                Vector3 returnVec;
+
+                float outOfBoundMag = (sensitivity / (diffMag - sensitivity));
+                returnVec = diff.normalized * (speed * (outOfBoundMag / sensitivity));
+
+                return returnVec;
+            }
+            else
+            {
+                return Flee(target, me, speed);
+            }
+        }
+
+        // Predictive steering
+        /* None of these have actually bee correctly implemented as it wasn't needed for the prototype.
+		 * If you do need to implement these refer to the Mat Buckland examples
+		*/
+        public static Vector3 Pursuit(Vector3 target, Vector3 me, float speed)
+        {
+            return (me - target).normalized;
+        }
+
+        public static Vector3 Evade(Vector3 target, Vector3 me, float speed)
+        {
+            return (me - target).normalized;
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager m_GameManager = null;
+    private static GameObject m_PlayerReference = null;
+	private static List<GameObject> m_EnemyAiList = null;
+
+    public void Awake()
+    {
+        m_PlayerReference = GameObject.Find("Player");
+		m_EnemyAiList = new List<GameObject> ();
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag ("EnemyAi");
+		foreach (GameObject enemy in enemies)
+		{
+			m_EnemyAiList.Add (enemy);
+		}
+    }
+
+    static public GameManager GetInstance()
+    {
+        if (m_GameManager == null) m_GameManager = new GameManager();
+        return m_GameManager;
+    }
+
+    static public GameObject GetPlayer()
+    {
+        return m_PlayerReference;
+    }
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.RightControl))
+		{
+			foreach (GameObject enemy in m_EnemyAiList)
+			{
+				if (enemy == null)
+				{
+					m_EnemyAiList.Remove (enemy);
+					m_EnemyAiList.TrimExcess ();
+				}
+				Health hp = enemy.GetComponent<Health>();
+				if (hp != null)
+				{
+					hp.TakeDamage (10);
+				}
+			}
+		}
+	}
+}
